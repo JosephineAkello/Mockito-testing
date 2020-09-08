1b9da4e729c01505f6ee6930ab89e253e246a88b
JosephineAkello <josephineakello2030@gmail.com>
2020-09-08 16:45:07 +0300

    add stub test cases

---
+++ src/test/java/com/example/Mockitotesting/business/SomeBusinessImplStubTest.java
@@ -0,0 +1,39 @@
+package com.example.Mockitotesting.business;
+
+import com.example.Mockitotesting.data.SomeDataService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SomeDataServiceStub implements SomeDataService{
+
+@Override
+    public int[] retrieveAllData(){
+    return new int[] {1,2,3};
+}
+}
+class SomeBusinessImplStubTest {
+
+    @Test
+    void calculateSumUsingDataService_basic() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        business.setSomeDataService(new SomeDataServiceStub());
+        int actualResults = business.calculateSumUsingDataService();
+        int expectedResults = 6;
+        assertEquals(expectedResults, actualResults);
+    }
+    @Test
+    void calculateSumUsingDataService_empty() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResults = business.calculateSumUsingDataService();
+        int expectedResults = 0;
+        assertEquals(expectedResults, actualResults);
+    }
+    @Test
+    void calculateSumUsingDataService_onevalue() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResults = business.calculateSumUsingDataService();
+        int expectedResults = 5;
+        assertEquals(expectedResults, actualResults);
+    }
+}
