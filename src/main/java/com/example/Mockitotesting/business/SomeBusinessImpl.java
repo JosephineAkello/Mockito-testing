bff106669404904980acad3c222cc9f29b9256ab
JosephineAkello <josephineakello2030@gmail.com>
2020-09-08 15:29:18 +0300

    add somedata logic

--- src/main/java/com/example/Mockitotesting/business/SomeBusinessImpl.java
+++ src/main/java/com/example/Mockitotesting/business/SomeBusinessImpl.java
@@ -1,9 +1,29 @@
 package com.example.Mockitotesting.business;
 
+import com.example.Mockitotesting.data.SomeDataService;
+
 public class SomeBusinessImpl {
+
+    private SomeDataService someDataService;
+
+    public void setSomeDataService(SomeDataService someDataService) {
+        this.someDataService = someDataService;
+    }
+
+    public SomeBusinessImpl() {
+    }
 
     public int calculateSum(int[] data){
         int sum = 0;
+        for(int value:data){
+            sum += value;
+        }
+        return sum;
+    }
+
+    public int calculateSumUsingDataService(){
+        int sum = 0;
+        int[] data = someDataService.retrieveAllData();
         for(int value:data){
             sum += value;
         }
