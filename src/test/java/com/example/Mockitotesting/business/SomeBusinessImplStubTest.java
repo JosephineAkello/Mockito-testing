4ba219080eb202efc8dddc4687e2b2e6c0de6042
JosephineAkello <josephineakello2030@gmail.com>
2020-09-08 16:58:42 +0300

    add stub test cases

--- src/test/java/com/example/Mockitotesting/business/SomeBusinessImplStubTest.java
+++ src/test/java/com/example/Mockitotesting/business/SomeBusinessImplStubTest.java
@@ -5,35 +5,55 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SomeDataServiceStub implements SomeDataService{
+class SomeDataServiceStub implements SomeDataService {
 
-@Override
-    public int[] retrieveAllData(){
-    return new int[] {1,2,3};
-}
-}
-class SomeBusinessImplStubTest {
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{1, 2, 3};
+    }
 
-    @Test
-    void calculateSumUsingDataService_basic() {
-        SomeBusinessImpl business = new SomeBusinessImpl();
-        business.setSomeDataService(new SomeDataServiceStub());
-        int actualResults = business.calculateSumUsingDataService();
-        int expectedResults = 6;
-        assertEquals(expectedResults, actualResults);
+    class SomeDataServiceEmptyStub implements SomeDataService {
+        @Override
+        public int[] retrieveAllData() {
+            return new int[]{};
+        }
+
+        class SomeDataServiceOneElementStub implements SomeDataService {
+
+            @Override
+            public int[] retrieveAllData() {
+                return new int[]{5};
+            }
+        }
     }
-    @Test
-    void calculateSumUsingDataService_empty() {
-        SomeBusinessImpl business = new SomeBusinessImpl();
-        int actualResults = business.calculateSumUsingDataService();
-        int expectedResults = 0;
-        assertEquals(expectedResults, actualResults);
-    }
-    @Test
-    void calculateSumUsingDataService_onevalue() {
-        SomeBusinessImpl business = new SomeBusinessImpl();
-        int actualResults = business.calculateSumUsingDataService();
-        int expectedResults = 5;
-        assertEquals(expectedResults, actualResults);
+
+    class SomeBusinessImplStubTest {
+
+        @Test
+        void calculateSumUsingDataService_basic() {
+            SomeBusinessImpl business = new SomeBusinessImpl();
+            business.setSomeDataService(new SomeDataServiceStub());
+            int actualResults = business.calculateSumUsingDataService();
+            int expectedResults = 6;
+            assertEquals(expectedResults, actualResults);
+        }
+
+        @Test
+        void calculateSumUsingDataService_empty() {
+            SomeBusinessImpl business = new SomeBusinessImpl();
+            business.setSomeDataService(new SomeDataServiceEmptyStub());
+            int actualResults = business.calculateSumUsingDataService();
+            int expectedResults = 0;
+            assertEquals(expectedResults, actualResults);
+        }
+
+        @Test
+        void calculateSumUsingDataService_onevalue() {
+            SomeBusinessImpl business = new SomeBusinessImpl();
+            business.setSomeDataService(new SomeDataServiceOneElementStub());
+            int actualResults = business.calculateSumUsingDataService();
+            int expectedResults = 5;
+            assertEquals(expectedResults, actualResults);
+        }
     }
 }
