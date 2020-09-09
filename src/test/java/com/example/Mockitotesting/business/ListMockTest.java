7bdc7bc0fc7b7accc0491a0c4928c330382e7e28
JosephineAkello <josephineakello2030@gmail.com>
2020-09-09 17:33:24 +0300

    add verify method calls scenarios

--- src/test/java/com/example/Mockitotesting/business/ListMockTest.java
+++ src/test/java/com/example/Mockitotesting/business/ListMockTest.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -38,9 +42,13 @@ public class ListMockTest {
     @Test
     public void verifiation_basics(){
     String value = mock.get(0);
+    String value1 = mock.get(1);
 
-    verify(mock.get(0));
-    verify(mock.get(anyInt()));
-    verify(mock,times(1)).get(anyInt());
+    verify(mock).get(0);
+    verify(mock, times(2)).get(anyInt());
+    verify(mock,atLeast(1)).get(anyInt());
+    verify(mock,atLeastOnce()).get(anyInt());
+    verify(mock,atMost(2)).get(anyInt());
+    verify(mock,never()).get(2);
     }
 }
