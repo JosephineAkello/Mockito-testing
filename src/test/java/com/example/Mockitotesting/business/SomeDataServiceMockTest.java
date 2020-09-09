841ed06880dae5511a00cb85e2e082921c064917
JosephineAkello <josephineakello2030@gmail.com>
2020-09-09 17:14:06 +0300

    use mock on multiple return values

--- src/test/java/com/example/Mockitotesting/business/SomeDataServiceMockTest.java
+++ src/test/java/com/example/Mockitotesting/business/SomeDataServiceMockTest.java
@@ -3,27 +3,28 @@ package com.example.Mockitotesting.business;
 import com.example.Mockitotesting.data.SomeDataService;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class SomeDataServiceMockTest  {
 
+    @InjectMocks
     SomeBusinessImpl business = new SomeBusinessImpl();
-    SomeDataService dataServiceMock = mock(SomeDataService.class);
 
-
-    @Before
-    public void before(){
-        business.setSomeDataService(dataServiceMock);
-    }
+    @Mock
+    SomeDataService dataServiceMock;
 
         @Test
         void calculateSumUsingDataService_basic() {
             when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{1, 2, 3});
-            int expectedResults = 6;
-            assertEquals(expectedResults, business.calculateSumUsingDataService());
+            assertEquals(6, business.calculateSumUsingDataService());
         }
 
         @Test
