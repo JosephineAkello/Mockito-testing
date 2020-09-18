c14549bd85b7defd00af6d7b3a8e89464d0bdab8
JosephineAkello <josephineakello2030@gmail.com>
2020-09-18 04:37:12 +0300

    add business layer method

--- src/main/java/com/example/Mockitotesting/controller/ItemController.java
+++ src/main/java/com/example/Mockitotesting/controller/ItemController.java
@@ -1,16 +1,28 @@
 package com.example.Mockitotesting.controller;
 
 import com.example.Mockitotesting.model.Item;
-import org.springframework.stereotype.Controller;
+import com.example.Mockitotesting.service.ItemBusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
 public class ItemController {
 
+
+    @Autowired
+    private ItemBusinessService businessService;
+
+
     @GetMapping("/dummy-item")
     public Item Dummy_items(){
+
         return new Item(1,"Baaay", 10, 100);
+    }
+
+    @GetMapping("/item-fom-business-service")
+    public Item ItemFromBusinessService(){
+        return businessService.retrieveHadCodedItem();
     }
 }
 
