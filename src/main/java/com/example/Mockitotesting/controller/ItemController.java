01454e08d16f75f7dae570961399c6f4b10b0578
JosephineAkello <josephineakello2030@gmail.com>
2020-09-22 22:33:31 +0300

    add contoller method

--- src/main/java/com/example/Mockitotesting/controller/ItemController.java
+++ src/main/java/com/example/Mockitotesting/controller/ItemController.java
@@ -1,5 +1,7 @@
 package com.example.Mockitotesting.controller;
 
+import java.util.List;
+
 import com.example.Mockitotesting.model.Item;
 import com.example.Mockitotesting.service.ItemBusinessService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,11 @@ public class ItemController {
     @GetMapping("/item-fom-business-service")
     public Item ItemFromBusinessService(){
         return businessService.retrieveHadCodedItem();
+    }
+
+    @GetMapping("/all-items-from-the-database")
+    public List<Item>  retrieveAllItems(){
+        return businessService.retrieveAllItems();
     }
 }
 
