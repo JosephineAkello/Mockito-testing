77f1c81569fc325710d5b1885939be76a2a2da69
JosephineAkello <josephineakello2030@gmail.com>
2020-09-22 22:32:04 +0300

    add data to be inserted

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-insert into item(id,name,price,quantity)
-values(1000, 'Item',10,100);
+insert into item(id,name,price,quantity) values(1000, 'Item',10,100);
+insert into item(id,name,price,quantity) values(1001, 'Item1',6,10);
+insert into item(id,name,price,quantity) values(1002, 'Item2',15,200);
