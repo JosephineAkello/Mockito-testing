a266a6cbcd371997de250219c4f6e21fe34cba6a
JosephineAkello <josephineakello2030@gmail.com>
2020-09-22 22:34:15 +0300

    modify the business logic

--- src/main/java/com/example/Mockitotesting/service/ItemBusinessService.java
+++ src/main/java/com/example/Mockitotesting/service/ItemBusinessService.java
@@ -1,11 +1,28 @@
 package com.example.Mockitotesting.service;
 
+import java.util.List;
+
+import com.example.Mockitotesting.data.ItemRepository;
 import com.example.Mockitotesting.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemBusinessService {
+
+    @Autowired
+    private ItemRepository repository;
+
     public Item retrieveHadCodedItem() {
         return new Item(1,"Baaay", 10, 100);
     }
+
+    public List<Item> retrieveAllItems(){
+        List<Item> items = repository.findAll();
+        for(Item item:items){
+            item.setValue(item.getPrice() * item.getQuantity());
+        }
+       return repository.findAll();
+    }
+
 }
